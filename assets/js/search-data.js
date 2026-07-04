52ef3bd1df0f85c0b6d21a0b844837ed6dab6b0e
BradleyWang0416 <95471245+BradleyWang0416@users.noreply.github.com>
2026-07-04 09:40:12 +0000

    Deploying to gh-pages from @ vitpkusz/vit.pkusz.github.io@9341cd283ed7102b6af560d812de0dd614ea871b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-lab",
-    title: "Lab",
+    id: "nav-home",
+    title: "Home",
     section: "Navigation",
     handler: () => {
       window.location.href = "/vit.pkusz.github.io/";
