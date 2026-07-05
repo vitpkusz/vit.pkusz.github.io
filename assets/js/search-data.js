a2f61da206fc92d83b76b836dc390324524d6529
BradleyWang0416 <95471245+BradleyWang0416@users.noreply.github.com>
2026-07-05 09:26:16 +0000

    Deploying to gh-pages from @ vitpkusz/vitpkusz.github.io@f295ce18e0fc9f7e08beda88f689c5ec64efadae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,28 +7,28 @@ ninja.data = [{
     title: "Lab",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/vit.pkusz.github.io/";
+      window.location.href = "/";
     },
   },{id: "nav-publications",
           title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/vit.pkusz.github.io/publications/";
+            window.location.href = "/publications/";
           },
         },{id: "nav-awards",
           title: "Awards",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/vit.pkusz.github.io/awards/";
+            window.location.href = "/awards/";
           },
         },{id: "nav-team",
           title: "Team",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/vit.pkusz.github.io/team/";
+            window.location.href = "/team/";
           },
         },{id: "post-a-post-with-plotly-js",
         
@@ -38,14 +38,14 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/vit.pkusz.github.io/blog/2025/plotly/";
+            window.location.href = "/blog/2025/plotly/";
           
         },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/vit.pkusz.github.io/books/the_godfather/";
+              window.location.href = "/books/the_godfather/";
             },},{id: "news-prof-zhou-won-the-nsf-career-award",
           title: 'Prof. Zhou won the NSF CAREER Award.',
           description: "",
@@ -128,47 +128,47 @@ ninja.data = [{
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/vit.pkusz.github.io/projects/1_project/";
+              window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/vit.pkusz.github.io/projects/2_project/";
+              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/vit.pkusz.github.io/projects/3_project/";
+              window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/vit.pkusz.github.io/projects/4_project/";
+              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/vit.pkusz.github.io/projects/5_project/";
+              window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/vit.pkusz.github.io/projects/6_project/";
+              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/vit.pkusz.github.io/projects/7_project/";
+              window.location.href = "/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/vit.pkusz.github.io/projects/8_project/";
+              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/vit.pkusz.github.io/projects/9_project/";
+              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
